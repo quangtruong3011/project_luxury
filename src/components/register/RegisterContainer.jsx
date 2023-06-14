0715783b3cd3f76d8c374c3fb37acb5f6646e50d
hija971 <hieupham.211197@gmail.com>
2023-06-14 19:23:29 +0700

    update

--- src/components/register/RegisterContainer.jsx
+++ src/components/register/RegisterContainer.jsx
@@ -1,34 +1,47 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
 function RegisterContainer() {
+  const [email, setEmail] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
 
-        const [email, setEmail] = useState('');
-        const [isValidEmail, setIsValidEmail] = useState(true);
-      
-        const handleEmailChange = (event) => {
-          const enteredEmail = event.target.value;
-          setEmail(enteredEmail);
-        };
-      
-        const validateEmail = () => {
-            const emailRegex = /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
-            const isValid = emailRegex.test(email);
-            setIsValidEmail(isValid);
-          };
-    
+  const handleEmailChange = (event) => {
+    const enteredEmail = event.target.value;
+    setEmail(enteredEmail);
+  };
+
+  const validateEmail = () => {
+    const emailRegex =
+      /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i;
+    const isValid = emailRegex.test(email);
+    setIsValidEmail(isValid);
+  };
+
   return (
     <div>
-      <form className="flex flex-col items-center" action="login">
-        <h1
-          className="text-5xl font-semibold"
-          style={{ fontFamily: "Hind Siliguri" }}
-        >
-          REGISTER FORM
-        </h1>
-        <input type="text" placeholder="User name*" />
-        <input type="email" value={email} onChange={handleEmailChange} placeholder="Email*"/>
+      <form
+        className="mx-auto flex w-96 flex-col items-center bg-blue-500"
+        style={{ fontFamily: "Hind" }}
+        action="register"
+      >
+        <h1 className="text-5xl font-semibold">REGISTER FORM</h1>
+        <input
+          className="userNameInputBox h-10 w-full border-2 border-solid border-white bg-transparent px-4"
+          type="text"
+          placeholder="User name*"
+        />
+        <input
+          className="emailInputBox h-10 w-full border-2 border-solid border-white bg-transparent px-4"
+          type="email"
+          value={email}
+          onChange={handleEmailChange}
+          placeholder="Email*"
+        />
         {!isValidEmail && <p className="text-red-500">Invalid email</p>}
-        <input type="password" placeholder="Password*" />
+        <input
+          className="passwordInputBox h-10 w-full border-2 border-solid border-white bg-transparent px-4"
+          type="password"
+          placeholder="Password*"
+        />
         <button className="text-lg" onClick={validateEmail}>
           LOGIN
         </button>
