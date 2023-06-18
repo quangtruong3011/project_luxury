5bf34d2e278d3c8f61133e5323ead6c381cd919f
hija971 <hieupham.211197@gmail.com>
2023-06-18 21:31:36 +0700

    update-components-for-main

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      screens: {
+        'sm': '576px',
+        'md': '768px',
+        'lg': '992px',
+        'xl': '1200px',
+        '2xl': '1400px',
+      },
+    },
   },
   plugins: [],
 }
