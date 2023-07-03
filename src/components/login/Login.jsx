bb9867579ad16eb3b4731127f9fccf2c110f2518
hija971 <hieupham.211197@gmail.com>
2023-07-04 01:04:14 +0700

    api-popUp-update

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -8,11 +8,11 @@ import "./Login.css";
 
 function Login() {
   const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
+    username: undefined,
+    password: undefined,
   });
 
-  const { user, loading, error, dispatch } = useContext(AuthContext);
+  const { loading, error, dispatch } = useContext(AuthContext);
 
   const navigate = useNavigate();
 
@@ -20,13 +20,12 @@ function Login() {
     setCredentials((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
   
-
   const handleClick = async (e) => {
     e.preventDefault();
     dispatch({ type: "LOGIN_START" });
     try {
       const res = await axios.post("/auth/login", credentials);
-      dispatch({ type: "LOGIN_SUCCESS", payload: res.data.details });
+      dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
       navigate("/")
     } catch (err) {
       dispatch({ type: "LOGIN_FAILURE", payload: err.response.data });
